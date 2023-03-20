9c955f3df9f915c353267a3c78213e658aa08bda
Siqi Chen <siqi1987@hotmail.com>
2023-03-20 12:10:36 +0100

    write example data

--- components/ProductCard/index.js
+++ components/ProductCard/index.js
@@ -1,14 +1,31 @@
 import styled from "styled-components";
 
+//each product card component
 export default function ProductCard() {
-  return <StyledProductCard>1234</StyledProductCard>;
+  return (
+    <StyledProductCard>
+      <div>
+        <img src="../img/beispielsfoto.png" alt="photo" width="180px" height="180px"/>
+      </div>
+      <div>
+        <h2>Fahrrad</h2>
+        <p>
+          Ich verkaufe das Fahrrad meiner Tochter, da es zu klein geworden ist.
+          Guter Zustand!
+        </p>
+        <div>50€</div>
+        <div>shoppingcart</div>
+
+      </div>
+    </StyledProductCard>
+  );
 }
 
 const StyledProductCard = styled.div`
-box-shadow: rgba(0, 0, 0, 0.2) 0px 10px 30px 0px;
-width:240px;
-height:360px;
-border-radius:20px;
-margin:20px;
+  box-shadow: rgba(0, 0, 0, 0.2) 0px 10px 30px 0px;
+  width: 240px;
+  height: 360px;
+  border-radius: 20px;
+  margin: 20px;
 `;
 //background-color:#97EAEE
