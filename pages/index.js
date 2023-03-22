3999bc370b82c388f3d5b566a23cd41479f2540c
Siqi Chen <siqi1987@hotmail.com>
2023-03-22 18:23:28 +0100

    write a function to add product in shopping cart

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,29 @@ import PRODUCT_DATA from "../pages/api/productsdaten.js";
 export default function HomePage() {
   const [productData, setProductData] = useState(PRODUCT_DATA);
 
+  // state for shopping cart:(products, total amount, total price)
+  const [cartData, setCartData] = useState({
+    products: [],
+    totalAmount: 0,
+    totalPrice: 0,
+  });
+  // add product in the shopping cart:
+  const addProductHandler = (product) => {
+    const newCart = { ...cartData };
+    // if the product already exists in the shopping cart
+    if (newCart.items.indexOf(product) < 0) {
+      newCart.products.push(product);
+      product.amount = 1;
+    } else {
+      product.amount += 1;
+    }
+
+    newCart.totalAmount += 1;
+    newCart.totalPrice += product.price;
+
+
+  };
+
   return (
     <>
       <Layout />
