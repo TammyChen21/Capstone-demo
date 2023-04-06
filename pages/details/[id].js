c8382cf71f9bff97c8d75b77565ce37a851df1a8
Siqi Chen <siqi1987@hotmail.com>
2023-04-06 20:58:05 +0200

    add styling for details page

--- pages/details/[id].js
+++ pages/details/[id].js
@@ -4,7 +4,9 @@ import { useRouter } from "next/router";
 import { ProductData } from "../../pages/_app";
 import Layout from "../../components/Layout";
 import styled from "styled-components";
-import Image from "next/image";
+import FavoriteButton from "../../components/FavoriteButton";
+import AddButton from "../../components/AddButton";
+import PRODUCT_DATA from "../api/productsdaten";
 
 export default function DetailsPage() {
   const productData = useContext(ProductData);
@@ -12,9 +14,7 @@ export default function DetailsPage() {
   const id = router.query.id;
 
   let selectedProduct = null;
-   selectedProduct = productData.find(
-    (productcard) => productcard.id === id
-  );
+  selectedProduct = productData.find((productcard) => productcard.id === id);
   console.log(productData);
 
   if (!selectedProduct) {
@@ -25,38 +25,82 @@ export default function DetailsPage() {
     <>
       <Layout />
       <StyledDetailsPage>
-        <StyledTitle>{selectedProduct.title}</StyledTitle>
-        <div>{selectedProduct.price}</div>
-        <div>{selectedProduct.description}</div>
+        <StyledProductContainer>
+          <StyledImage
+            src={selectedProduct.image}
+            alt="photo"
+            width="280px"
+            height="280px"
+          />
+          <StyledTitle>{selectedProduct.title}</StyledTitle>
+          <StyledDes>{selectedProduct.description}</StyledDes>
+          <StyledPrice>{selectedProduct.price}</StyledPrice>
+          <StyledButtonCountainer>
+          <StyledFavoriteButton><FavoriteButton /></StyledFavoriteButton>
+          <StyledAddButton><AddButton productcard={selectedProduct}/></StyledAddButton>
+          </StyledButtonCountainer>
+        </StyledProductContainer>
       </StyledDetailsPage>
     </>
   );
 }
 
-const StyledDetailsPage = styled.article`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
-  max-width: 612px;
-  height: 100%;
-  @media (min-width: 1024px) {
-    width: 100%;
-    max-width: 1200px;
-    flex-direction: row;
-    align-items: flex-start;
-    gap: 50px;
+const StyledDetailsPage = styled.div``;
+const StyledProductContainer = styled.div`
+box-shadow: rgba(0, 0, 0, 0.2) 0px 10px 30px 0px;
+width: 80%;
+margin-top:80px;
+margin-left:10%;
+border-radius: 25px;
+display: flex;
+flex-direction: column;
+align-items: center;
+border:solid;
+
+`;
+
+const StyledImage = styled.div`
+border-radius: 20px;
+  margin-top: 5px;
+  box-shadow: 4px 4px rgba(0, 0, 0, 0.2);
+  margin-bottom: -15px;
+`;
+
+const StyledTitle = styled.h2`
+color: #004056;
+  margin-bottom: 8px;
+  margin-left: 10%;
+  margin-right: 10px;`;
+
+const StyledDes = styled.h3`
+color: #004056;
+font-size: 1.2rem;
+margin-left: 10%;
+margin-bottom: -8px;`;
+const StyledPrice = styled.div`
+margin-top: 30px;
+  color: #fc8663;
+  margin-right: 60%;
+  font-weight: 500;
+  font-size: 1.5rem;
+  ::after {
+    content: "€";
   }
 `;
+const StyledButtonCountainer=styled.div`
+display:flex;
+justify-content: space-around;
+border:solid;
+width:40%;
 
-const StyledImage = styled(Image)`
-  width: 100%;
-  height: auto;
-  object-fit: cover;
-  transform: scale(1.1);
-  object-position: 50% 50%;
-  margin-top: 10px;
-`;
+`
 
-const StyledTitle = styled.p``;
+const StyledFavoriteButton=styled.button`
+width:40px;
+height:40px;`
+const StyledAddButton=styled.button`
+width:40px;
+height:40px;
+`
+
 
